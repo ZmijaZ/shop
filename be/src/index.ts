575a838909a62a14708b2d7c3a7324302fa351e7
ZmijaZ <zmijaz2001@gmail.com>
2024-09-04 11:55:43 +0200

    Mongo connection established

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,14 +1,31 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = 3000;
+
+mongoose.set('strictQuery', false);
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+console.log('connection to', MONGODB_URI);
+
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB', error.message)
+    })
 
 app.get('/', (req, res) => {
     res.send('Checking')
 })
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
